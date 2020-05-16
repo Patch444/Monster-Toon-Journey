7d90be9defb47d4370e9acbf8a73572dc7766c29
Patrick McDaniels <mcdan113@mail.chapman.edu>
2020-05-15 20:28:14 -0700

    Implemented Checkpoint Prefab

--- MonsterToonJourney/Assets/Scripts/Checkpoint.cs
+++ MonsterToonJourney/Assets/Scripts/Checkpoint.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     PlayerMove player;
+
     public bool beenReached;
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         beenReached = false;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,6 +25,8 @@ public class Checkpoint : MonoBehaviour
         {
             player.spawnPosition = this.transform.position;
             beenReached = true;
+            anim.Play("Checkpoint_Activate");
+            
         }
     }
 }
